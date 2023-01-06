3464827cadca639fc40bffa11ce4fb6a48e94ead
Kaloyan Draganov <kaloyanddraganov@gmail.com>
2023-01-06 09:44:56 +0200

    Implemented automatic theme switching.

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,60 @@ import 'package:flutter/cupertino.dart';
 import 'widgets/HomePage.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const StandUpAssist());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class StandUpAssist extends StatefulWidget {
+  const StandUpAssist({Key? key}) : super(key: key);
+
+  @override
+  State<StandUpAssist> createState() => _StandUpAssistState();
+}
+
+class _StandUpAssistState extends State<StandUpAssist>
+    with WidgetsBindingObserver {
+  Brightness? _brightness;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance?.addObserver(this);
+    _brightness = WidgetsBinding.instance?.window.platformBrightness;
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance?.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangePlatformBrightness() {
+    if (mounted) {
+      setState(() {
+        _brightness = WidgetsBinding.instance?.window.platformBrightness;
+      });
+    }
+
+    super.didChangePlatformBrightness();
+  }
+
+  CupertinoThemeData get _lightTheme => const CupertinoThemeData(
+        brightness: Brightness.light,
+        primaryColor: CupertinoColors.black,
+      );
+
+  CupertinoThemeData get _darkTheme => const CupertinoThemeData(
+        brightness: Brightness.dark,
+        primaryColor: CupertinoColors.white,
+      );
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(
+    return CupertinoApp(
       title: 'Stand Up Assist',
-      theme: CupertinoThemeData(
-        primaryColor: CupertinoColors.white,
-        brightness: Brightness.dark,
-      ),
-      home: HomePage(title: 'Stand Up Assist'),
+      theme: _brightness == Brightness.dark ? _darkTheme : _lightTheme,
+      home: const HomePage(title: 'Stand Up Assist'),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:stand_up_assist/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const StandUpAssist());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
