9a0ef5976bf52c244331a7297042f48f67fd5543
smjonas <jonas.strittmatter@gmx.de>
2023-01-08 15:15:49 +0100

    Initial android version

--- android/app/src/main/kotlin/com/kaloyanddraganov/stand_up_assist/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.kaloyanddraganov.stand_up_assist
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:stand_up_assist/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const StandUpAssist());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
